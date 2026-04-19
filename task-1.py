58f197638260aa4a7b7d244616afbabc877965d5
SrinithiShanmugam <srinithi289sri@gmail.com>
2026-04-19 11:57:29 +0530

    Add files via upload

---
+++ task-1.py
@@ -0,0 +1,60 @@
+#hello world
+print("Hello world!")
+#add,sub,mul,div,modolous
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+#square root
+print(a**2)
+#quadratic
+print((a+b)**2)
+print((a-b)**2)
+#swap 3rd variable
+c=a
+a=b
+b=c
+print(a,b)
+
+#swap without 3rd variable
+s=20
+r=10
+s=s-r
+r=s+r
+print(s,r)
+#km
+km=3
+m=km*0.63
+print(m)
+
+#celsius
+cel=33
+form=(cel*1.8)+32
+print(form)
+#last num
+num=332
+lastnum=num%10
+print(lastnum)
+#last two num
+num1=3337
+last_num=num1%100
+print(last_num)
+
+#middle value and its square
+nums=input("enter num")
+mid=(int(nums[3]))
+squ=mid*mid
+print(squ)
+
+      
+
+
+
+
+
+
+
+
